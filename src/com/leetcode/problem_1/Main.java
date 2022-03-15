b5c89c945d5ecc7d83ba2d2ea835ae2e15f4fd3b
Ilia Kovalenko <kovalenko@bezlimit.ru>
2022-03-15 08:41:45 +0200

    problem 1

---
+++ src/com/leetcode/problem_1/Main.java
@@ -0,0 +1,29 @@
+package com.leetcode.problem_1;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 7, 11, 15};
+        int target = 18;
+
+        Solution sol = new Solution();
+        int[] result = sol.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
