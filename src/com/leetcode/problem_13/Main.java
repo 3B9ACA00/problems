6dab1414b9812637a62969ee3cee44d3bb522492
Ilia Kovalenko <kovalenko@bezlimit.ru>
2022-03-16 23:45:39 +0200

    problem 13

---
+++ src/com/leetcode/problem_13/Main.java
@@ -0,0 +1,49 @@
+package com.leetcode.problem_13;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("problem 13:");
+
+        Solution sol = new Solution();
+        System.out.println(sol.romanToInt("III"));
+        System.out.println(sol.romanToInt("LVIII"));
+        System.out.println(sol.romanToInt("MCMXCIV"));
+        System.out.println(sol.romanToInt("DCXXI"));
+    }
+}
+
+
+class Solution {
+    public int romanToInt(String s) {
+        char[] charArr = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        int[] values = {1, 5, 10, 50, 100, 500, 1000};
+        int result = 0;
+        int nextPos, pos;
+        char ch, nextCh;
+
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+            pos = new String(charArr).indexOf(ch);
+
+            if (i + 1 < s.length()) {
+                if (ch == 'I' || ch == 'X' || ch == 'C') {
+                    nextCh = s.charAt(i + 1);
+                    if (nextCh != 'I') {
+                        nextPos = new String(charArr).indexOf(nextCh);
+                        result += values[nextPos] - values[pos];
+                        i++;
+                        continue;
+                    }
+                }
+            }
+
+            result += values[pos];
+
+            System.out.print(ch);
+            System.out.print(" - ");
+            System.out.println(result);
+        }
+
+        return result;
+    }
+}
