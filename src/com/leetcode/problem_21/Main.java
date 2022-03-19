612e200ac81d9d8bf2d2d14fab06b277552d312c
Ilya Kovalenko <kovalenko-ilia@yandex.com>
2022-03-19 15:08:14 +0200

    problem 21

---
+++ src/com/leetcode/problem_21/Main.java
@@ -0,0 +1,46 @@
+package com.leetcode.problem_21;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("problem 21:");
+        Solution sol = new Solution();
+
+        // Input: list1 = [1,2,4], list2 = [1,3,4]
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode list = sol.mergeTwoLists(list1, list2);
+
+        //ListNode list = sol.mergeTwoLists(new ListNode(), new ListNode());
+
+        while (list != null) {
+            System.out.println(list.val);
+            list = list.next;
+        }
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list2.next, list1);
+            return list2;
+        }
+    }
+}
+
